d1ea78bb6ddf2d0442ccd32ae032b6979dc150bc
Docile123 <imbereyemasoo@gmail.com>
2025-05-12 23:43:48 +0300

    feat: the footer component is added

--- src/pages/StudentList.jsx
+++ src/pages/StudentList.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import styles from './student.module.css';
 
 import useStudentStore from "../store/studentStore";
 
@@ -14,7 +15,7 @@ const StudentList = () => {
   if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
 
-  return <div>Student List</div>;
+  return <div className={styles.container}>Student List</div>;
 };
 
 export default StudentList;
