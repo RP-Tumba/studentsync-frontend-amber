3e129b635b71dc6b8bd576fa8a76c6a38feb5108
Irakoze Pacifique <irakozepacific3@gmail.com>
2025-05-16 19:01:21 +0200

    completed feature Nav Bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import style from './Nav.module.css';
+import { Link } from 'react-router-dom';
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className={style.header}>
+          <div className={style.head1}>STUDENTSYNC</div>
+          <div className={style.head2}>
+            <Link to="/home">Home</Link>
+          </div>
+          <div className={style.head3}>
+            <Link to="/Add">Add Student</Link>
+          </div>
+          <div className={style.head4}>
+            <Link to="/All">
+              <button>All Student</button>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
